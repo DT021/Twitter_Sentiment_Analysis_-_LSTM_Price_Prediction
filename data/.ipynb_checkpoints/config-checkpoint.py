b2dc1a68d24caefbb43cc97a51802c988be826d8
Toufic Lawand <tlawand@users.noreply.github.com>
2020-07-07 19:35:27 -0400

    updated price data and added initial LSTM model

--- data/.ipynb_checkpoints/config-checkpoint.py
+++ data/.ipynb_checkpoints/config-checkpoint.py
@@ -3,10 +3,10 @@ import os
 # constants
 API_KEY = os.getenv('ALPACA_API_KEY')
 SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
-# API_BASE_URL = 'https://paper-api.alpaca.markets/'
 API_DATA_URL = 'https://data.alpaca.markets'
 ALPACA_WS_URL = 'wss://data.alpaca.markets/stream'
-# POLYGON_WS_URL = 'wss://alpaca.socket.polygon.io/stocks	'
+
+ALPHA_VANTAGE_API = os.getenv('ALPHA_VANTAGE_API_KEY')
 
 HEADERS = {
     'APCA-API-KEY-ID': API_KEY,
