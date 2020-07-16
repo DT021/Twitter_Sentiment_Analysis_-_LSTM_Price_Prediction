35d9f5eda3eba796676f0b6f717233075e736c5e
Shubhra <bhatnagar@gmail.com>
2020-07-16 16:32:38 -0400

    Added os path

--- Toufic/price_predictions.py
+++ Toufic/price_predictions.py
@@ -4,10 +4,11 @@ import time, sys, asyncio
 from streamz import Stream
 from streamz.dataframe import DataFrame
 
-sys.os.path.abspath(__file__)
+import os
+basepath = os.path.abspath(os.path.dirname(__file__)) + "/"
 
 def combined_predictions():
-    combined_df = pd.read_csv('price_data.csv')
+    combined_df = pd.read_csv(basepath + 'price_data.csv')
     return combined_df
 
 stream = Stream()
